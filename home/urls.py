9aacca6f91862d1c0da41e5600bd53a026c48225
Cengiz YILMAZ <terabilgi@gmail.com>
2020-05-01 03:02:02 +0300

    Menü Sayfaları Eklendi

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('hakkimizda/', views.hakkimizda, name='hakkimizda'),
+    path('referanslar/', views.referanslar, name='referanslar'),
+    path('iletisim/', views.iletisim, name='iletisim'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 
 from .models import Setting
 
+setting = Setting.objects.get(pk=1)
 
 def index(request):
     # metin = 'Ali'
     # return HttpResponse('Merhaba %s' % metin)
-    setting = Setting.objects.get(pk=1)
-
-    context = {'setting':setting}
+    context = {'setting':setting,'page':'home'}
     return render(request, 'index.html', context)
+
+def hakkimizda(request):
+    context = {'setting':setting}
+    return render (request,'hakkimizda.html',context)
+
+def referanslar(request):
+    context = {'setting': setting}
+    return render(request,'referanslar.html',context)
+
+def iletisim(request):
+    context = {'setting': setting}
+    return render(request,'iletisim.html',context)
+
 
